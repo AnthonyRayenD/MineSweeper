64ab3bb15799b925c4725aae8e3a32e10951cad4
DarrelIVO <darrel.r@incentivio.com>
2024-11-11 03:02:11 +0530

    Create test class

---
+++ src/test/java/com/minesweeper/service/MineSweeperServiceTest.java
@@ -0,0 +1,24 @@
+package com.minesweeper.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MineSweeperServiceTest {
+
+    @Test
+    void createBoard() {
+    }
+
+    @Test
+    void printBoard() {
+    }
+
+    @Test
+    void showBlock() {
+    }
+
+    @Test
+    void hasMine() {
+    }
+}
